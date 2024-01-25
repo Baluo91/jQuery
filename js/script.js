f86ef079df61dba6b263d95913f42c2dfe98361f
Dávid Balog <strawhog91@gmail.com>
2024-01-25 20:07:52 +0100

    jQuery

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    // sem budeme písať jQuery kód
+});
+
+//* 11. Nauč se jQuery a rozpohybuj webové stránky - FadeTo a průhlednost, rozdíl FadeTo a FadeOut
+
+$(function() {
+    // $(".red-box").fadeTo(2000, 0.0) // zmizne ale na pozadí akoby je
+    // $(".red-box").fadeOut(2000) // zmizne (aj na pozadí) a posunie ostatné.
+});
