727d284dbe4c726b632cb5e6c4705de7d9da5471
DavidBalog <strawhog91@gmail.com>
2024-01-28 17:23:08 +0100

    slide down / up / toggle

--- js/script.js
+++ js/script.js
@@ -14,11 +14,22 @@ $(function() {
 
     // $(".red-box").fadeToggle(2000), fadeToggle(2000); // toggle - prepínač - východzí stav toggle prepne a ďalší toggle ho prepne tiež.
 
-    $(".red-box").hide(2000); //box sa skríva po dobu 2 sekúnd
-    $(".red-box").show(2000); //box sa zobrazuje po dobu 2 sekúnd
+    // $(".red-box").hide(2000); //box sa skríva po dobu 2 sekúnd
+    // $(".red-box").show(2000); //box sa zobrazuje po dobu 2 sekúnd
     //? to isté, ako hide a show spraví nasledujúci zápis : 
     // $(".red-box").toggle(2000)
     // $(".red-box").toggle(2000)
 
+    //* 13. - SlideUp, slideDown a slideToggle
+    $(".red-box").slideUp(2000);
+    $(".red-box").slideDown(2000);
+    $(".blue-box").slideUp(3000);
+    $(".blue-box").slideDown(3000);
+    $(".green-box").slideUp(4000);
+    $(".green-box").slideDown(4000);
 
+    $(".green-box").slideToggle(4000); //? toggle to isté ako up down
+
+    $("p").hide();
+    $("p").slideDown(1000);
 });
