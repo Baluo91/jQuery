cf54ce641ae1834ddbdaa9cfb7dd587e78ae2466
DavidBalog <strawhog91@gmail.com>
2024-01-28 20:47:23 +0100

    children parent parents

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(function() {
     // sem budeme písať jQuery kód
 });
 
-//* 23. Výběr elementů: běžný tagu, class a id - prvý a posledný element - párne a nepárne
-//? ako vybrať z li elementov napr. ten nepárny?
-//! $("li:even") alebo párny $("li:odd")
+//* 25. Výběr elementů: Potomci a rodiče
+//?  .find("li") = z #list nájdi všetky li a zmeň im farbu
+//?  miesto .find vieme použiť .children .parent . parents (obalí všetkých rodičov)
+//!  $("li:first").parent("") zafarbí li 1 a . parent zoberie celé ul a zafarbí ho 
 
 $(function() {
-    $("li:odd").css("background-color","rgba(150,150,40,0.8)");
-    
+    $("li:first").parent("").css("background-color","rgba(150,150,40,0.8");
 });
