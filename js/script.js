6a89e70fde3190d0e917b99822680cc1acbe1c5b
DavidBalog <strawhog91@gmail.com>
2024-01-28 19:40:59 +0100

    reťazenie pomocou .

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@ $(function() {
     // sem budeme písať jQuery kód
 });
 
-//* 18. Časování animací pomocí delay
+//* 19. Chaining neboli řetězení
 
 $(function() {
-    $(".red-box").fadeTo(1000,0.2); // 80% priehľadnosť
-    $(".blue-box").delay(1000).fadeTo(1000,0.5); // .delay(1000) bude čakať 1 sekundu a potom sa prevedie animácia
-    $(".green-box").delay(2000).fadeTo(1000,0.6); //.delay(2000) bude čakať 2 sekundy a potom sa prevedie animácia
+    $(".blue-box").delay(2000).fadeTo(1000,0.4).fadeOut(2000).delay(1000).fadeIn(2000);
 });
-/*opacity = nepriehľadnosť*/
+
