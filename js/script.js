2a1dcfb9f7553c7e5bc98ef2fffe5719f8a7c10a
DavidBalog <strawhog91@gmail.com>
2024-01-28 20:03:43 +0100

    Výběr elementů: běžný tagu, class a id zmena css farby

--- js/script.js
+++ js/script.js
@@ -2,24 +2,10 @@ $(function() {
     // sem budeme písať jQuery kód
 });
 
-//* 20. Callback funkce 
-// volá sa , keď animácie skončia
+//* 21. Výběr elementů: běžný tagu, class a id
 
-// $(function() {
-//     $(".red-box").fadeTo(2000,0,function(){
-//         alert("Toto sa stane po animácii");
-//     });
-// });
-
-//? tento zápis - najprv red box fadeTo a po ňom blue box
-//* používame call back funkciu aj miesto delay. 
-//* pred poslednou  zátvorkou ) pridáme ,function(){ a sem píšeme ďalšiu --- $(".red-box").fadeTo(2000,0,function(){}  */
 $(function() {
-    $(".red-box").fadeTo(2000,0,function(){
-        $(".blue-box").fadeTo(2000,0,function(){
-            $(".green-box").fadeTo(2000,0);
-        });
-    });
+    $("p").css("background-color","rgba(150,150,40,0.8)");
+    $(".red-box").css("background-color","rgba(150,150,40,0.8)");
+    $("#list").css("background-color","rgba(150,150,40,0.8)");
 });
-
-//! všetky 3 boxy postupne zmiznú - fadeTo 
