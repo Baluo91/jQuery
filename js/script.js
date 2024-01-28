b0bd18004fbaba111affe8b4d9819f2f56f33db4
DavidBalog <strawhog91@gmail.com>
2024-01-28 18:49:24 +0100

    animácie

--- js/script.js
+++ js/script.js
@@ -2,34 +2,15 @@ $(function() {
     // sem budeme písať jQuery kód
 });
 
-//* 11. Nauč se jQuery a rozpohybuj webové stránky - FadeTo a průhlednost, rozdíl FadeTo a FadeOut
+//* 14. Začínáme s animacemi
+$(function() {
+    $(".red-box").animate({});
+});
 
 $(function() {
-    // $(".red-box").fadeTo(2000, 0.0) // zmizne ale na pozadí akoby je
-
-    // $(".red-box").fadeOut(2000) // zmizne (aj na pozadí) a posunie ostatné.
-    // $(".red-box").fadeIn(2000) // objaví sa a posunie ostatné.
-
-    // $(".red-box").fadeOut(2000), fadeIn(2000); // kratší zápis
-
-    // $(".red-box").fadeToggle(2000), fadeToggle(2000); // toggle - prepínač - východzí stav toggle prepne a ďalší toggle ho prepne tiež.
-
-    // $(".red-box").hide(2000); //box sa skríva po dobu 2 sekúnd
-    // $(".red-box").show(2000); //box sa zobrazuje po dobu 2 sekúnd
-    //? to isté, ako hide a show spraví nasledujúci zápis : 
-    // $(".red-box").toggle(2000)
-    // $(".red-box").toggle(2000)
-
-    //* 13. - SlideUp, slideDown a slideToggle
-    $(".red-box").slideUp(2000);
-    $(".red-box").slideDown(2000);
-    $(".blue-box").slideUp(3000);
-    $(".blue-box").slideDown(3000);
-    $(".green-box").slideUp(4000);
-    $(".green-box").slideDown(4000);
-
-    $(".green-box").slideToggle(4000); //? toggle to isté ako up down
-
-    $("p").hide();
-    $("p").slideDown(1000);
+    $(".red-box").animate({
+        "margin-left":"250px", // posunie z default na 250px 
+        "margin-left":"+=400px" // default je 50 vďaka += posunie z 50 na 450px 
+        
+    },4000); //? čas sa nastavuje medzi zátvorkami 
 });
