14a51f8b96a1fce331337de0d2dbf2250d23ca47
Dávid Balog <strawhog91@gmail.com>
2024-01-29 11:15:30 +0100

    modulo

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@ $(function() {
     // sem budeme písať jQuery kód
 });
 
-//* 26. Výběr elementů: next a preview 
-//?   $("li:first").next() - vyberie druhý li element
-//?   $("li:last").prev() - vyberie predposledný li element
+//* 28. Výběr elementů: filter - modulo 
+// TODO: zbytok po celočíselnom delení 
+/*
+5 modulo 2 = 1 (2 sa zmestí do 5 2x a ostane 1)
+6 modulo 2 = 2 (2 sa zmestí do 6 3x a ostane 0)
+12 modulo 5 = 2 (5 sa zmestí do 12 2x a ostane 2)
+14 modulo 5 = 4 (5 sa zmestí do 14 2x a ostane 4)
+*/
+//?   modulo sa označuje znakom % 6 % 2 = 0
+//?   
 //!   
 
 $(function() {
-    // $("li:last").prev().css("background-color","rgba(150,150,40,0.8");
-    $("li:last").prev().next().css("background-color","rgba(150,150,40,0.8"); // dá sa to aj reťaziť = .prev().next()
+    $("li").filter(function(index){
+        return index % 2 === 0;
+    }).css("background-color","rgba(150,150,40,0.8");
 });
+
+
