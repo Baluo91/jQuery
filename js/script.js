40502bfeddea695cab1b0a00f901ef8ea3b0d4a8
Dávid Balog <strawhog91@gmail.com>
2024-01-29 10:44:31 +0100

    siblings & header

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(function() {
     // sem budeme písať jQuery kód
 });
 
-//* 25. Výběr elementů: Potomci a rodiče
-//?  .find("li") = z #list nájdi všetky li a zmeň im farbu
-//?  miesto .find vieme použiť .children .parent . parents (obalí všetkých rodičov)
-//!  $("li:first").parent("") zafarbí li 1 a . parent zoberie celé ul a zafarbí ho 
+//* 26. Výběr elementů: siblings & nadpisy
+//?  .siblings("") vyberie siblings na rovnakej úrovni (všetky div, p, ul ...)
+//?  .siblings("p") vyberie siblings p
+//!   .siblings(":header") vyberie všetky nadpisy h1 h2 ...
 
 $(function() {
-    $("li:first").parent("").css("background-color","rgba(150,150,40,0.8");
+    $("#list").siblings(":header").css("background-color","rgba(150,150,40,0.8");
 });
